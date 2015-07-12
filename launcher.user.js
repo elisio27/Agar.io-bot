93827b7c17c7ca857f41a681bb77f2a79ab6da43
elisio27 <gang.pu@free.fr>
2015-07-12 18:20:01 +0200

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -1218,7 +1218,7 @@ console.log("Running Bot Launcher!");
                 dArc = [],
                 dText = [],
                 lines = [],
-                names = ["NotReallyABot"],
+                names = ["Elisio#PaC"],
                 originalName = names[Math.floor(Math.random() * names.length)],
                 sessionScore = 0,
                 serverIP = "",
